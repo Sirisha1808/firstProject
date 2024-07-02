a6d0ea023eee54beaa27a90d0ea2a8a1ead72746
unknown <allurisirisha5@gmail.com>
2024-07-02 18:07:51 +0530

    first commit

---
+++ src/main/java/Sample/Orange.java
@@ -0,0 +1,28 @@
+package Sample;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Orange {
+
+    public static void main (String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Wait up to 10 seconds
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Thread.sleep(5000);
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123", Keys.ENTER);
+        System.out.println("Navigate to Dashboard:" + driver.getTitle());
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
